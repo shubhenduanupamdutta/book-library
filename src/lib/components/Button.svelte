49c8122a80e045660e16955f826c7b4129e54360
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-22 15:17:58 +0530

    Button Section

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -45,3 +45,40 @@
 		{@render children()}
 	</button>
 {/if}
+
+<style>
+	a {
+		display: block;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+	.btn {
+		padding: 0.7rem 1.4rem;
+		min-width: 13rem;
+		text-align: center;
+		background-color: black;
+		border-radius: 0.7rem;
+		color: white;
+		border: 1px solid white;
+		font-weight: normal;
+		font-size: 1.28rem;
+	}
+
+	.btn-secondary {
+		background-color: white;
+		color: black;
+		border: 1px solid black;
+	}
+
+	.btn-danger {
+		background-color: rgb(136, 4, 4);
+	}
+
+	.btn-menu {
+		min-width: 8.75rem;
+		padding: 0.47rem 1.17rem;
+	}
+</style>
--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import heroImage from '$assets/hero.png';
+	import { Button } from '$components';
 </script>
 
 <section class="default-margin hero">
@@ -14,7 +15,7 @@
 			Our app offers a beautiful designed, easy-to-use interface that makes managing your book
 			collection a joy.
 		</h4>
-		<button>Sign up</button>
+		<Button href="/register">Sign Up</Button>
 	</div>
 	<img class="hero-image" src={heroImage} alt="" />
 </section>
